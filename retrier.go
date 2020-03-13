30bbda9db38a940b54bd353ead6c968974832b6d
J. Santos <jamillo@gmail.com>
2020-03-13 03:09:49 -0300

    :sparkles: Implement Retrier

---
+++ retrier.go
@@ -0,0 +1,108 @@
+package services
+
+import (
+	"context"
+	"time"
+)
+
+// ServiceRetrier wraps a `Service` in order to provide functionality for retrying in case of its starting process
+// fails.
+type ServiceRetrier struct {
+	service          Service
+	tries            uint
+	timeout          time.Duration
+	waitBetweenTries time.Duration
+}
+
+// RetrierBuilder is the helper for building `ServiceRetrier`.
+type RetrierBuilder struct {
+	tries            uint
+	timeout          time.Duration
+	waitBetweenTries time.Duration
+}
+
+// Retrier returns a new `RetrierBuilder` instance.
+func Retrier() *RetrierBuilder {
+	return &RetrierBuilder{
+		tries: 3,
+	}
+}
+
+// Build creates a new `ServiceRetrier` with
+func (builder *RetrierBuilder) Build(service Service) *ServiceRetrier {
+	return &ServiceRetrier{
+		service:          service,
+		timeout:          builder.timeout,
+		tries:            builder.tries,
+		waitBetweenTries: builder.waitBetweenTries,
+	}
+}
+
+// Tries sets the tries for the `Retrier`.
+func (builder *RetrierBuilder) Tries(value uint) *RetrierBuilder {
+	builder.tries = value
+	return builder
+}
+
+// Timeout set the timeout for the `Retrier`.
+func (builder *RetrierBuilder) Timeout(value time.Duration) *RetrierBuilder {
+	builder.timeout = value
+	return builder
+}
+
+// WaitBetweenTries set the timeout for the `Retrier`.
+func (builder *RetrierBuilder) WaitBetweenTries(value time.Duration) *RetrierBuilder {
+	builder.waitBetweenTries = value
+	return builder
+}
+
+// Name will return a human identifiable name for this service. Ex: Postgresql Connection.
+func (retrier *ServiceRetrier) Name() string {
+	return retrier.service.Name()
+}
+
+// Stop will stop this service.
+//
+// For most implementations it will be blocking and should return only when the service finishes stopping.
+//
+// If the service is successfully stopped, `nil` should be returned. Otherwise, an error must be returned.
+func (retrier *ServiceRetrier) Stop() error {
+	return retrier.service.Stop()
+}
+
+// StartWithContext implements the logic of starting a service. If it fails, it should use the configuration to retry.
+func (retrier *ServiceRetrier) StartWithContext(ctx context.Context) error {
+	if retrier.timeout > 0 {
+		ctx2, cancel := context.WithTimeout(ctx, retrier.timeout)
+		ctx = ctx2
+		defer cancel()
+	}
+	for i := uint(0); i < retrier.tries; i++ {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			// This make the select not to block.
+		}
+
+		if service, ok := retrier.service.(StartableWithContext); ok {
+			err := service.StartWithContext(ctx)
+			if err == nil {
+				// If started ...
+				return nil
+			} else if ctx.Err() != nil {
+				// If the starting process was cancelled...
+				return ctx.Err()
+			}
+		} else if service, ok := retrier.service.(Startable); ok {
+			err := service.Start()
+			if err == nil {
+				return nil
+			}
+		}
+		if retrier.waitBetweenTries > 0 {
+			time.Sleep(retrier.waitBetweenTries)
+		}
+	}
+	return ErrExhaustedAttempts
+}
---
+++ retrier_test.go
@@ -0,0 +1,122 @@
+package services
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
+)
+
+var _ = Describe("Retrier", func() {
+	Context("Startable", func() {
+		It("should start and stop a service", func() {
+			serviceA := &serviceStart{
+				name: "A",
+			}
+			serviceARetrier := Retrier().Tries(3).Build(serviceA)
+			Expect(serviceARetrier.Name()).To(Equal("Service A"))
+			starter := NewStarter(serviceARetrier)
+			Expect(starter.Start()).To(Succeed())
+			Expect(serviceA.startCallCount).To(Equal(1))
+			Expect(serviceA.started).To(BeTrue())
+			Expect(starter.Stop()).To(Succeed())
+			Expect(serviceA.stopped).To(BeTrue())
+		})
+
+		It("should start a service after the third try waiting between each", func() {
+			serviceA := &serviceStart{
+				startErr: []error{errors.New("any error"), errors.New("any error")},
+			}
+			serviceARetrier := Retrier().Tries(3).WaitBetweenTries(time.Millisecond * 100).Build(serviceA)
+			starter := NewStarter(serviceARetrier)
+			startedAt := time.Now()
+			Expect(starter.Start()).To(Succeed())
+			Expect(time.Since(startedAt)).To(BeNumerically("~", time.Millisecond*200, time.Millisecond*20))
+			Expect(serviceA.startCallCount).To(Equal(3))
+			Expect(serviceA.started).To(BeTrue())
+		})
+
+		It("should fail starting a service after reaching the tries limit", func() {
+			serviceA := &serviceStart{
+				startErr: []error{errors.New("any error"), errors.New("any error"), errors.New("any error")},
+			}
+			serviceARetrier := Retrier().Tries(3).Build(serviceA)
+			starter := NewStarter(serviceARetrier)
+			err := starter.Start()
+			Expect(err).To(HaveOccurred())
+			Expect(errors.Is(ErrExhaustedAttempts, err)).To(BeTrue())
+			Expect(serviceA.startCallCount).To(Equal(3))
+			Expect(serviceA.started).To(BeFalse())
+		})
+
+		It("should fail when a service takes too long to start", func() {
+			serviceA := &serviceStart{
+				startErr:   []error{errors.New("any error"), errors.New("any error"), errors.New("any error")},
+				startDelay: time.Millisecond * 100,
+			}
+			serviceARetrier := Retrier().Tries(3).Timeout(time.Millisecond * 150).Build(serviceA)
+			starter := NewStarter(serviceARetrier)
+			err := starter.Start()
+			Expect(err).To(HaveOccurred())
+			Expect(errors.Is(context.DeadlineExceeded, err)).To(BeTrue())
+			Expect(serviceA.startCallCount).To(Equal(2))
+			Expect(serviceA.started).To(BeFalse())
+		})
+	})
+
+	Context("StartableWithContext", func() {
+		It("should start and stop a service", func() {
+			serviceA := &serviceStartWithContext{
+				name: "A",
+			}
+			serviceARetrier := Retrier().Tries(3).Build(serviceA)
+			Expect(serviceARetrier.Name()).To(Equal("Service With Context A"))
+			starter := NewStarter(serviceARetrier)
+			Expect(starter.Start()).To(Succeed())
+			Expect(serviceA.startCallCount).To(Equal(1))
+			Expect(serviceA.started).To(BeTrue())
+			Expect(starter.Stop()).To(Succeed())
+			Expect(serviceA.stopped).To(BeTrue())
+		})
+
+		It("should start a service after the third try", func() {
+			serviceA := &serviceStartWithContext{
+				startErr: []error{errors.New("any error"), errors.New("any error")},
+			}
+			serviceARetrier := Retrier().Tries(3).Build(serviceA)
+			starter := NewStarter(serviceARetrier)
+			Expect(starter.Start()).To(Succeed())
+			Expect(serviceA.startCallCount).To(Equal(3))
+			Expect(serviceA.started).To(BeTrue())
+		})
+
+		It("should fail starting a service after reaching the tries limit", func() {
+			serviceA := &serviceStartWithContext{
+				startErr: []error{errors.New("any error"), errors.New("any error"), errors.New("any error")},
+			}
+			serviceARetrier := Retrier().Tries(3).Build(serviceA)
+			starter := NewStarter(serviceARetrier)
+			err := starter.Start()
+			Expect(err).To(HaveOccurred())
+			Expect(errors.Is(ErrExhaustedAttempts, err)).To(BeTrue())
+			Expect(serviceA.startCallCount).To(Equal(3))
+			Expect(serviceA.started).To(BeFalse())
+		})
+
+		It("should fail when a service takes too long to start", func() {
+			serviceA := &serviceStartWithContext{
+				startErr:   []error{errors.New("any error"), errors.New("any error"), errors.New("any error")},
+				startDelay: time.Millisecond * 100,
+			}
+			serviceARetrier := Retrier().Tries(3).Timeout(time.Millisecond * 150).Build(serviceA)
+			starter := NewStarter(serviceARetrier)
+			err := starter.Start()
+			Expect(err).To(HaveOccurred())
+			Expect(errors.Is(context.DeadlineExceeded, err)).To(BeTrue())
+			Expect(serviceA.startCallCount).To(Equal(2))
+			Expect(serviceA.started).To(BeFalse())
+		})
+	})
+})
