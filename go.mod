3708b27e7a9ee32881dee5f258f6bbd45607e31d
J. Santos <jamillo@gmail.com>
2021-07-05 22:46:28 -0300

    chore(deps): Update tools.go

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/setare/services
 go 1.16
 
 require (
-	github.com/novln/macchiato v1.0.2
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.13.0
 	github.com/pkg/errors v0.9.1
 	github.com/setare/go-os-signals v0.1.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- services_test.go
+++ services_test.go
@@ -3,12 +3,11 @@ package services_test
 import (
 	"testing"
 
-	"github.com/novln/macchiato"
-	"github.com/onsi/ginkgo"
-	"github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 func TestServices(t *testing.T) {
-	gomega.RegisterFailHandler(ginkgo.Fail)
-	macchiato.RunSpecs(t, "Services Tests")
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Services Tests")
 }
